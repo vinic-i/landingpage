2b1da8a246f6fc60d6b1e40ffd73777640f41e0f
Vinicius Henrique <vinicius.henrique@ufrn.br>
2024-11-15 23:53:22 -0300

    fix favicon

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BzbrsVgX.js","_app/immutable/chunks/disclose-version.DDs4AXTB.js","_app/immutable/chunks/runtime.CIO7fPzX.js","_app/immutable/chunks/legacy.DOurymHu.js","_app/immutable/chunks/store.B4C88Ypa.js","_app/immutable/chunks/entry.Duvk5MMV.js"];
+export const imports = ["_app/immutable/nodes/1.DGXWrISf.js","_app/immutable/chunks/disclose-version.DDs4AXTB.js","_app/immutable/chunks/runtime.CIO7fPzX.js","_app/immutable/chunks/legacy.DOurymHu.js","_app/immutable/chunks/store.B4C88Ypa.js","_app/immutable/chunks/entry.Dnvx2DSH.js"];
 export const stylesheets = [];
 export const fonts = [];
